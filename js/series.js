fa68b40baffbd16e3e80af1cccf9d0aed0476cc2
jief <jiefxxx@gmail.com>
2016-06-18 21:17:59 +0200

    Real first commit

---
+++ js/series.js
@@ -0,0 +1,55 @@
+var app = angular.module('Series', []);
+app.factory('GET', function($q) {
+  return{
+    series: function(){
+      var deferred = $q.defer();
+      $.ajax({
+        url: 'http://127.0.0.1:8888/media/series',
+        type: 'get',
+        dataType: 'json',
+        success: function (data) {
+          console.log(data)
+          deferred.resolve(data.series);
+          }
+        });
+      return deferred.promise;
+    },
+    serie: function(name){
+      var deferred = $q.defer();
+      $.ajax({
+        url: 'http://127.0.0.1:8888/media/series/'+name,
+        type: 'get',
+        dataType: 'json',
+        success: function (data) {
+          console.log(data)
+          deferred.resolve(data);
+          //$scope.show($scope.series[0])
+        }
+      });
+      return deferred.promise;
+    }
+  }
+})
+
+app.controller('seriesCtrl',function($scope,$window,GET){
+  GET.series().then(function(series) {
+      $scope.series = series;
+      $scope.show(series[0])
+    }, function(error) {
+      console.log(error)
+      $scope.error = error;
+      $scope.series = [];
+  });
+
+  $scope.show = function(name){
+    GET.serie(name).then(function(data) {
+        //console.log(data.name)
+        $scope.data_show = data;
+        //console.log($scope.show_name)
+      }, function(error) {
+        console.log(error)
+        $scope.error = error;
+        $scope.show_data = {};
+    });
+  };
+});
---
+++ js/torrent.js
@@ -0,0 +1,78 @@
+var app = angular.module('myApp', []);
+app.controller('urlCtrl',function($scope,$interval){
+
+  $scope.refresh_torrent = function(){
+    $.ajax({
+      url: 'http://127.0.0.1:8888/tm/info',
+      type: 'get',
+      dataType: 'json',
+      success: function (data) {
+         $scope.names = data.torrents;
+      }
+    });
+  };
+  $scope.add = function (){
+    if($scope.url.match(/^magnet:.*/) != null) {
+      //alert('Link is magnet');
+      $scope.add_from_magnet($scope.url)
+    }
+    else if($scope.url.match(/^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/i) != null) {
+      //alert('Link is url');
+      $scope.add_from_url($scope.url)
+    }
+    else if($scope.url[0] == '/'){
+      alert('Link is file (not working)');
+    }
+    else{
+      //alert('Nothing find');
+    }
+  }
+  $scope.add_from_magnet = function (magnet) {
+    $.ajax({
+      url: 'http://127.0.0.1:8888/tm/add',
+      type: 'post',
+      dataType: 'json',
+      success: function (data) {
+        console.log(data);
+         $scope.url = "";
+      },
+      data: '{"magnet":"'+magnet+'"}'
+    });
+  };
+  $scope.add_from_url = function (url) {
+    $.ajax({
+      url: 'http://127.0.0.1:8888/tm/add',
+      type: 'post',
+      dataType: 'json',
+      success: function (data) {
+        console.log(data);
+         $scope.url = "";
+      },
+      data: '{"url":"'+url+'"}'
+    });
+  };
+  $scope.torrent_action = function(id,action){
+    console.log({"id":id,"action":action})
+    $.ajax({
+      url: 'http://127.0.0.1:8888/tm/torrent_action',
+      type: 'post',
+      dataType: 'json',
+      data: '{"id":'+id+',"action":"'+action+'"}'
+    });
+  };
+  $scope.get_class = function(state){
+    if(state=='downloading'){
+      return 'success'
+    }
+    else if(state == 'seeding'){
+      return 'warning'
+    }
+    else{
+      return ""
+    }
+  };
+  $scope.refresh_torrent();
+  $interval(function () {
+        $scope.refresh_torrent();
+    }, 1000);
+});
